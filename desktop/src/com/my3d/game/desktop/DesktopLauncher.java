1f4e2fc0cad79c2eff3177d0ccf56486652eeef3
cdgira <cdgira@cs.ship.edu>
2016-07-19 15:00:31 -0400

    Adding rest of the initial files for the libgdx project.

---
+++ desktop/src/com/my3d/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.my3d.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.my3d.game.My3dGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new My3dGame(), config);
+	}
+}
